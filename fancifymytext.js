a9bb71c8a67706311012989d4ded0f036c656e44
Kevin <Kspottier@yahoo.com>
2024-03-22 14:27:55 -0700

    Update fancifymytext.js
    javaScript with Functions and Listeners to handle clicks, text changes, ect.

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,1 +1,38 @@
+//function for bigger buttons click
+function makeBigger(){
+    document.getElementById("textInput").style.fontSize = "24pt";
+}
+//function for radio buttons
+function changeStyle(){
+    var textArea = document.getElementById("textInput");
+    var fancyRadio = document.getElementById("fancyRadio");
+    if(fancyRadio.checked){
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
 
+    }
+    else{
+        textArea.style.fontWeight = "normal";
+        textArea.style.color = "black";
+        textArea.style.textDecoration = "none";
+    }
+}
+//Function to hangle moo 
+function makeMoo(){
+    var textArea = document.getElementById("textInput");
+    var text = textArea.ariaValueMax;
+    var sentences = text.split(". ");
+    for (var i = 0; i < sentences.length; i++) {
+        var words = sentences[i].split(" ");
+        var lastWordIndex = words.length - 1;
+        words[lastWordIndex] = words[lastWordIndex].toUpperCase() + "-Moo";
+        sentences[i] = words.join(" ");
+    }
+    textArea.value = sentences.join(". ");
+}
+//Event Listeners
+document.getElementById("biggerButton").addEventListener("click", makeBigger);
+document.getElementById("fancyRadio").addEventListener("change", changeStyle);
+document.getElementById("boringRadio").addEventListener("change", changeStyle);
+document.getElementById("mooButton").addEventListener("click", makeMoo);
