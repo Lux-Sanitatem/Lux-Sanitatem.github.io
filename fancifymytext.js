ba938d3513394eb54667316a40bce8ae333d2735
Kevin <Kspottier@yahoo.com>
2024-03-22 15:35:48 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -5,14 +5,16 @@ function makeBigger(){
 //function for radio buttons
 function changeStyle(){
     var textArea = document.getElementById("textInput");
-    var fancyRadio = document.getElementById("fancyRadio");
-    if(fancyRadio.checked){
+    var fancyShmancyRadio = document.getElementById("fancyShmancyRadio");
+    var boringBettyRadio = document.getElementById("boringBettyRadio");
+    if(fancyShmancyRadio.checked){
         textArea.style.fontWeight = "bold";
         textArea.style.color = "blue";
         textArea.style.textDecoration = "underline";
 
     }
-    else{
+    else if (boringBettyRadio.checked) 
+    {
         textArea.style.fontWeight = "normal";
         textArea.style.color = "black";
         textArea.style.textDecoration = "none";
@@ -21,7 +23,7 @@ function changeStyle(){
 //Function to hangle moo 
 function makeMoo(){
     var textArea = document.getElementById("textInput");
-    var text = textArea.ariaValueMax;
+    var text = textArea.areaValueMax;
     var sentences = text.split(". ");
     for (var i = 0; i < sentences.length; i++) {
         var words = sentences[i].split(" ");
@@ -33,6 +35,6 @@ function makeMoo(){
 }
 //Event Listeners
 document.getElementById("biggerButton").addEventListener("click", makeBigger);
-document.getElementById("fancyRadio").addEventListener("click", changeStyle);
-document.getElementById("boringRadio").addEventListener("change", changeStyle);
+document.getElementById("fancyShmancyRadio").addEventListener("change", changeStyle);
+document.getElementById("boringBettyRadio").addEventListener("change", changeStyle);
 document.getElementById("mooButton").addEventListener("click", makeMoo);
